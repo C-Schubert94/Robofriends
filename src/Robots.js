b32a3d3c8abdbb49f486a9fb06d949468214e422
Chris Schubert <chris.schubert1994@gmail.com>
2022-04-14 12:49:56 -0400

    first commit

---
+++ src/Robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'John Doe',
+        username: 'Johnny',
+        email: 'johnny@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Jane Doe',
+        username: 'Jane',
+        email: 'jane@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Jim Smith',
+        username: 'Jimmy',
+        email: 'jimmy@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Chris Schubert',
+        username: 'Chris',
+        email: 'chris@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Destini Schubert',
+        username: 'Dessemo',
+        email: 'dessemo@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Justin Fredericks',
+        username: 'J Fred',
+        email: 'jfred@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Leonidas Schubert',
+        username: 'Leon',
+        email: 'leon@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Josie Fredericks',
+        username: 'Jojo',
+        email: 'jojo@gmail.com'
+    }
+]
